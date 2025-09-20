d894a19eb250f3c31cbcf781a0d6cd56d8f67ded
Elmassmelike <elmasmelike14@gmail.com>
2025-09-20 23:53:40 +0200

    cron.js added

--- src/config/cron.js
+++ src/config/cron.js
@@ -15,21 +15,21 @@ const job = new cron.CronJob("*/14 * * * *", function () {
 
 export default job;
 
-/*
-  CRON JOB EXPLANATION:
-  Cron jobs are scheduled tasks that run periodically at fixed intervals.
-  In this example, we want to send 1 GET request every 14 minutes
-  so that our API never gets inactive on Render.com.
 
-  How to define a "Schedule"?
-  You define a schedule using a cron expression, which consists of 5 fields representing:
+  //CRON JOB EXPLANATION:
+  //Cron jobs are scheduled tasks that run periodically at fixed intervals.
+  //In this example, we want to send 1 GET request every 14 minutes
+  //so that our API never gets inactive on Render.com.
 
-    MINUTE, HOUR, DAY OF THE MONTH, MONTH, DAY OF THE WEEK
+  //How to define a "Schedule"?
+  //You define a schedule using a cron expression, which consists of 5 fields representing:
 
-  ? EXAMPLES && EXPLANATION:
-    */14 * * * *   -> Every 14 minutes
-    0 0 * * 0      -> At midnight on every Sunday
-    30 3 15 * *    -> At 3:30 AM, on the 15th of every month
-    0 0 1 1 *      -> At midnight, on January 1st
-    0 * * * *      -> Every hour
-*/
+  //! MINUTE, HOUR, DAY OF THE MONTH, MONTH, DAY OF THE WEEK
+
+  //? EXAMPLES && EXPLANATION:
+  //* 14 * * * *    - Every 14 minutes
+  //* 0 0 * * 0     - At midnight on every Sunday
+  //* 30 3 15 * *   - At 3:30 AM, on the 15th of every month
+  //* 0 0 1 1 *     - At midnight, on January 1st
+  //* 0 * * * *     - Every hour
+
